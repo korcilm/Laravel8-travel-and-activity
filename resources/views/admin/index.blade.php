2e9f2421d44891006b911ab9e9af1122a437a8e3
DESKTOP-MDEFT09\Muhammet Korçil <korcilmuhammet@gmail.com>
2021-12-18 14:03:30 +0300

    admin template giydirildi

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.home')
+@extends('layouts.admin')
 @section('title','Laravel Gezi Aktivite Sitesi')
 @section('description')
-    Türkiye'nin en iyi aktivite sitesi
+Türkiye'nin en iyi aktivite sitesi
 @endsection
 @section('keywords','Kocaeli, Gezi, Aktivite,İstanbul')
 @section('content')
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[HomeController::class,'index']);
+Route::get('/admin',[App\Http\Controllers\Admin\HomeController::class,'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
